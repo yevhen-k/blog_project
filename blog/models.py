2984db1865b72da9f9ffda59b5d77ba666c7dbd3
krasnokutskyi (Євген Краснокутський) <krasnokutskyi@office.local>
2019-04-08 15:04:03 +0300

    crud operations over posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,3 +15,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # after post successfully created -> redirect to certain view
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # posts = [
 #     {
@@ -17,6 +22,8 @@ from .models import Post
 #     },
 # ]
 
+
+# Function-based views
 def home(request):
     # return HttpResponse('<h1>Blog Home</h1>')
     # context = {
@@ -31,3 +38,71 @@ def home(request):
 def about(request):
     # return HttpResponse('<h1>About Blog</h1>')
     return render(request, 'blog/about.html', context={'title': 'About Blog'})
+
+
+# Class-based views (list views, detail views, create views, update views, delete views...)
+
+class PostListView(ListView):
+    model = Post
+    # by default django looks for template at <app>/<model>_<viewtype>.html
+    # which is blog/post_list.html
+    # so we need to alter this behaviour
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # ordering asc with minus sign
+
+
+
+class PostDetailView(DetailView):
+    # by default django looks for template at <app>/<model>_<viewtype>.html!
+    # -> blog/post_detail.html
+    model = Post
+
+
+# to restrict access for unauthorized users we can use
+# @method_decorator(login_required, name='dispatch') for class
+# or mixins (must be placed as first argument in inheritance):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # by default django looks for template at <app>/<model>_<viewtype>.html!
+    # -> blog/post_form.html
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # every post has to have author
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# to restrict updating posts the one is not owner use UserPassesTestMixin
+# and override test_func() method
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # by default django looks for template at <app>/<model>_<viewtype>.html!
+    # -> blog/post_form.html
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # every post has to have author
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    # by default django looks for template at <app>/<model>_<viewtype>.html!
+    # -> post_confurm_delete.html
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
